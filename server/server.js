6b9d9c9dc92b9bc303597eaa7bd2a3833275a08b
PrairieWaltz <minnowvswhale@gmail.com>
2023-01-10 23:58:58 -0500

    envUpdate

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const Document = require("./Document");
+require("dotenv").config();
 
 const dbUrl =
-  process.env.DB_URL || "mongodb://127.0.0.1:27017/google-docs-clone";
+  process.env.DB_URL || "mongodb://localhost:27017/google-docs-clone";
 
 mongoose.set("strictQuery", false);
 mongoose
